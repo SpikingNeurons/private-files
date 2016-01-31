dca1249d728c4d8bbd8bdd75bee06a24baf5b898
praveenneuron <praveenneuron@gmail.com>
2016-01-31 01:45:12 +0100

    seed encryption got through ....check if AES gives coorect result for reality check

--- SEED_test/SEED_test.py
+++ SEED_test/SEED_test.py
@@ -30,22 +30,37 @@ def _print_data_to_hexstr(data):
 
 def _generate_fake_dataset():
     global g_plain_text, g_truth_cipher_text, g_keys, g_key
+
+    print('_generate_fake_dataset')
+
+    # seed
     np.random.seed(1234567)
 
     # plain text
     str_temp = np.random.bytes(g_num_traces*g_num_bytes_per_trace)
     g_plain_text = np.fromstring(str_temp, dtype=np.uint8)
+    print('')
+    print('....................................................................')
+    print('')
+    print('plain_text')
     _print_data_to_hexstr(g_plain_text[0:32])
 
     # keys
     str_temp = np.random.bytes(g_num_traces*g_num_bytes_per_trace)
     g_keys = np.fromstring(str_temp, dtype=np.uint8)
+    print('keys')
     _print_data_to_hexstr(g_keys[0:32])
 
     # key
     str_temp = np.random.bytes(g_num_bytes_per_trace)
     g_key = np.fromstring(str_temp, dtype=np.uint8)
+    print('key')
     _print_data_to_hexstr(g_key)
+    print('')
+    print('....................................................................')
+    print('')
+    print('')
+    print('')
 
 
 def ____code_to_blabla():
@@ -59,22 +74,47 @@ def ____code_to_blabla():
     _print_pointer(g_plain_text)
     print(g_plain_text[0:32])
 
+    ###
+    g_plain_text.tobytes('C') == g_plain_text.tobytes()
 
-def generate_cipher_text_from_hazmat():
-    from cryptography.hazmat.backends.openssl.backend import backend
-    from cryptography.hazmat.primitives.ciphers import algorithms, base, modes
+
+class ThirdPartyCryptography():
+
+    @staticmethod
+    def encrypt_seed(plain_text, key):
+        from cryptography.hazmat.backends.openssl.backend import backend
+        from cryptography.hazmat.primitives.ciphers import algorithms, base, modes
+        cipher = base.Cipher(
+            algorithms.SEED(key),
+            modes.ECB(),
+            backend
+        )
+        encryptor = cipher.encryptor()
+        ct = encryptor.update(plain_text)
+        ct += encryptor.finalize()
+        return ct
+
+    @staticmethod
+    def generate_cipher(plain_text, keys, algorithm):
+        if algorithm == 'SEED':
+            ct = ThirdPartyCryptography.encrypt_seed(plain_text.tobytes('C'), keys.tobytes('C'))
+            return np.fromstring(ct, dtype=np.uint8)
+        return None
 
 
 def main():
+    global g_plain_text, g_truth_cipher_text, g_keys, g_key
     import sys
     print(sys.version)
     _generate_fake_dataset()
-    generate_cipher_text_from_hazmat()
+    g_truth_cipher_text = ThirdPartyCryptography.generate_cipher(
+        g_plain_text,
+        g_key,
+        'SEED')
+    _print_data_to_hexstr(g_truth_cipher_text[0:32])
 
 
 if __name__ == "__main__":
     main()
-
-
 
 
