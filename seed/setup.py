a386122e6388d2a532589b4fc2102339d92cce95
Praveen <praveenneuron@gmail.com>
2016-12-05 23:53:54 +0100

    delete the seed code

--- seed/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
-import numpy as np
-
-setup(
-    extensions =[Extension("*", ["*.pyx"])],
-    cmdclass={'build_ext': Cython.Build.build_ext},
-)
